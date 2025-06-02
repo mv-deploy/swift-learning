5615d0f500d2254f5aaa3cc6e54955d7e0303418
Max <Volgin>
2025-06-02 19:45:57 +0400

    1.1_volgin

---
+++ 1.1_volgin/1.1.1_volgin/1.1.1_volgin.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// Переменные для профиля пользователя
+let firstName: String = "Макс"
+let lastName: String = "Волгин"
+var age: Int = 32
+let gender: String = "Мужской"
+var aboutMe: String = "Работаю web дизайнером и учу Swift"
+let isPublicProfile: Bool = true
+
+// Вывожу характеристики профиля пользователя
+print("Имя: \(firstName)")
+print("Фамилия: \(lastName)")
+print("Возраст: \(age)")
+print("Пол: \(gender)")
+print("О себе: \(aboutMe)")
+print("Публичный профиль: \(isPublicProfile)")
